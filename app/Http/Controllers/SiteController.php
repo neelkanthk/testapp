f83be07ca6ac5716367082a5cc4e2583463abe66
Neelkanth <neelkanthkaushik2014@gmail.com>
2016-06-07 04:18:08 +0000

    new push

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Hobby;
+use App\User;
+use App\UserProfile;
 use Validator;
 
 class SiteController extends Controller {
@@ -17,6 +19,7 @@ class SiteController extends Controller {
 
     public function store(Request $request) {
 //        dd($request->all());
+
         $this->validate($request, [
             'name' => 'required|unique:users|max:255',
             'email' => 'required|email',
@@ -31,6 +34,29 @@ class SiteController extends Controller {
                 'otherhobby' => 'required'
             ]);
         endif;
+
+        $filename = '';
+        if ($request->hasFile('image')) {
+            if ($request->file('image')->isValid()) {
+                $filename = time() . '-' . $request->file('image')->getClientOriginalName();
+                $destinationPath = public_path('uploads/');
+                //dd($destinationPath);
+                $request->file('image')->move($destinationPath, $filename);
+            }
+        }
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        //$user->save();
+
+        $profile = new UserProfile();
+//        dd($profile);
+        $profile->gender = $request->gender;
+        $profile->image = $filename;
+        dd($profile);
+        $user->userprofile()->save();
     }
 
 }
--- app/UserProfile.php
+++ app/UserProfile.php
@@ -4,7 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class UserProfile extends Model
-{
-    //
+class UserProfile extends Model {
+
+    protected $table = 'user_profile';
+    protected $fillable = ['gender', 'image'];
+    public function user() {
+        $this->belongsTo('App\User');
+    }
+
 }
